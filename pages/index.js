57565e35e7551c82e2e9019206195f30a22d8ee9
mmdHasan-yazdanPanah <mmdhsn.yp.gmail.com>
2020-05-26 16:21:44 +0430

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
 import axios from 'axios';
 import Link from 'next/link';
