73b2f2be447389cf0a769b36329288485ccdb64e
mmdHasan-yazdanPanah <mmdhsn.yp.gmail.com>
2020-05-26 16:24:47 +0430

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/Layout';
+import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import axios from 'axios';
 import Link from 'next/link';
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Layout from '../../components/layout';
 import axios from 'axios';
 import Head from 'next/head';
